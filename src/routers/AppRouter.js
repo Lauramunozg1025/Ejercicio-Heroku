f20325acc25aa41c3f2dc3a90430ee21cfa19639
Laura Muñoz <lacrimugi.l.c@gmail.com>
2022-01-26 20:56:03 -0500

    21 Enero

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from "../components/Navbar";
 import Container from "../containers/App";
 
 export default class AppRouter extends Component{
     render(){
         return(
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Container/>} />
                 </Routes>
